8fd16a64c7cfe0c2c2fa9e22380288b2e72dd19b
Sara Gallego Mendez <saragallegomendez10@gmail.com>
2025-11-14 00:35:53 +0100

    add notificationsController exerecise

--- loader/loader.controller.js
+++ loader/loader.controller.js
@@ -1,18 +1,41 @@
-import { buildLoader } from "./loader.view.js";
+import { getTweets } from "./tweet-list.model.js";
+import { buildTweet, buildEmptyTweets } from "./tweet-list.view.js";
 
-export function loaderController(loaderContainer) {
+export async function tweetListController(tweetContainer) {
+  let tweetsToShow = [];
+
+  try {
+    const startEvent = new CustomEvent('start-fetching-tweets')
+    tweetContainer.dispatchEvent(startEvent);
+    tweetsToShow = await getTweets();    
+  } catch (error) {
+    const errorEvent = new CustomEvent('error-fetching-tweets', {
+      detail: {
+        message: error.message
+      }
+    })
+    tweetContainer.dispatchEvent(errorEvent);
+  } finally {
+    const finishEvent = new CustomEvent('finish-fetching-tweets')
+    tweetContainer.dispatchEvent(finishEvent);
+  }
   
-  const show = () => {
-    loaderContainer.innerHTML = buildLoader();
+  if (tweetsToShow.length === 0) {
+    tweetContainer.innerHTML = buildEmptyTweets()
   }
 
-  const hide = () => {
-    loaderContainer.innerHTML = '';
-  }
+  tweetsToShow.forEach((tweet) => {
 
-  return {
-    show,
-    hide
-  };
+    const newTweet = document.createElement('div');
+    // el controlador obtiene el HTML del módulo encargado de generar la vista.
+    newTweet.innerHTML = buildTweet(tweet)
 
+    newTweet.addEventListener('click', (event) => {
+      // newTweet.remove();
+      event.currentTarget.remove();
+    })
+
+    // añadir el tweet al DOM
+    tweetContainer.appendChild(newTweet)
+  })
 }
