805b1d2c320989a75841ff7f466016d650cba9ef
Sara <saragallegomendez10@gmail.com>
2025-11-18 16:38:19 +0100

    hide notification after 5 segs

--- tweet-list/tweet-list.view.js
+++ tweet-list/tweet-list.view.js
@@ -2,9 +2,9 @@ export const buildTweet = (tweet) => {
   return `
     <h3>${tweet.handler} escribió el ${tweet.date}: </h3>
     <h4>${tweet.message}</h4>
-  `
-}
+  `;
+};
 
 export const buildEmptyTweets = () => {
-  return `<p>No hay tweets disponibles</p>`
-}
+  return `<img style="width: 400px" src="https://compote.slate.com/images/97d7753a-b5ae-408a-8fed-7ff68b4ce3b7.jpeg" />`;
+};
