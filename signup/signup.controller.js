9af791c54eb338a526a384a67c488baca1fb3937
Sara <saragallegomendez10@gmail.com>
2025-11-18 16:44:00 +0100

    show notifications using notification controller

--- signup/signup.controller.js
+++ signup/signup.controller.js
@@ -1,6 +1,8 @@
 export const signupController = (signupForm) => {
   signupForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    // let isFormValid = true;
+    const errors = [];
 
     const password = signupForm.querySelector("#password");
     const passwordConfirmation = signupForm.querySelector("#passwordConfirm");
@@ -10,13 +12,34 @@ export const signupController = (signupForm) => {
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     );
     if (!emailRegExp.test(email.value)) {
-      alert("ERROR, email incorrecto");
+      // isFormValid = false;
+      const validationErrorEvent = new CustomEvent("signup-validation-error", {
+        detail: {
+          message: "El email no es válido",
+          type: "error",
+        },
+      });
+      errors.push(validationErrorEvent);
     }
 
-    if (password.value === passwordConfirmation.value) {
-      alert("TODO OK");
-    } else {
-      alert("ERROR, passwords diferentes");
+    if (password.value !== passwordConfirmation.value) {
+      // isFormValid = false;
+      const validationErrorEvent = new CustomEvent("signup-validation-error", {
+        detail: {
+          message: "Las contraseñas no coinciden",
+          type: "error",
+        },
+      });
+      errors.push(validationErrorEvent);
+    }
+
+    errors.forEach((errorEvent) => {
+      signupForm.dispatchEvent(errorEvent);
+    });
+
+    // si no hay errores, llamo al api
+    if (errors.length === 0) {
+      // consumir api
     }
   });
 };
