b108d276f151145bf62ae8a220b7a0a5158254de
Sara <saragallegomendez10@gmail.com>
2025-11-18 16:42:48 +0100

    validate passwords exercise

---
+++ signup/signup.controller.js
@@ -0,0 +1,6 @@
+export const signupController = (signupForm) => {
+  signupForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("botón pulsado, toca validar");
+  });
+};
