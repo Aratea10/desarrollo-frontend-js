b4a707546d01a4beda8ca4b9623229ef91098f61
Sara <saragallegomendez10@gmail.com>
2025-11-30 18:35:21 +0100

    create tweets

---
+++ create-tweet/createTweet.controller.js
@@ -0,0 +1,11 @@
+import { createTweet } from "./createTweet.model.js";
+
+export const createTweetController = (createTweetForm) => {
+  createTweetForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const tweetContent = createTweetForm.querySelector("#tweetContent").value;
+
+    createTweet(tweetContent);
+  });
+};
