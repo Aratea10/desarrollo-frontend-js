ae4be827239a3db6cab581c34369f8ee30042c27
Sara <saragallegomendez10@gmail.com>
2025-11-30 18:30:02 +0100

    login user

--- signup/signup.controller.js
+++ signup/signup.controller.js
@@ -1,52 +1,51 @@
+import { constants } from "../utils/constants.js";
 import { createUser } from "./signup.model.js";
 
 export const signupController = (signupForm) => {
-
   signupForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const errors = []
+    const errors = [];
 
     const password = signupForm.querySelector("#password");
     const passwordConfirmation = signupForm.querySelector("#passwordConfirm");
     const email = signupForm.querySelector("#email");
 
-    const emailRegExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    const emailRegExp = new RegExp(constants.mailRegExp);
     if (!emailRegExp.test(email.value)) {
-      const validationErrorEvent = new CustomEvent('signup-validation-error', {
+      const validationErrorEvent = new CustomEvent("signup-validation-error", {
         detail: {
           message: "El email no es válido",
-          type: "error"
-        }
+          type: "error",
+        },
       });
-      errors.push(validationErrorEvent)
+      errors.push(validationErrorEvent);
     }
 
     if (password.value !== passwordConfirmation.value) {
-      const validationErrorEvent = new CustomEvent('signup-validation-error', {
+      const validationErrorEvent = new CustomEvent("signup-validation-error", {
         detail: {
           message: "Las contraseñas no coinciden",
-          type: "error"
-        }
+          type: "error",
+        },
       });
-      errors.push(validationErrorEvent)
+      errors.push(validationErrorEvent);
     }
 
-    errors.forEach(errorEvent => {
-      signupForm.dispatchEvent(errorEvent)
-    })
+    errors.forEach((errorEvent) => {
+      signupForm.dispatchEvent(errorEvent);
+    });
 
     // si no hay errores, llamo al api
     if (errors.length === 0) {
       try {
-        await createUser(email.value, password.value)
+        await createUser(email.value, password.value);
         alert("usuario creado correctametne");
         setTimeout(() => {
-          window.location.href = '/'
+          window.location.href = "/";
         }, 3000);
       } catch (error) {
-        alert(error.message)
+        alert(error.message);
       }
     }
-
-  })
-}
+  });
+};
