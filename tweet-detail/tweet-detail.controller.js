8a84822f817ec10b5b9e3133e9adc42c151b4087
Sara <saragallegomendez10@gmail.com>
2025-11-30 18:42:52 +0100

    show remove tweet button logic exercise

--- tweet-detail/tweet-detail.controller.js
+++ tweet-detail/tweet-detail.controller.js
@@ -1,10 +1,12 @@
-import { getTweetDetail } from "./tweet-detail.model.js";
+import { getTweetDetail, getUserData } from "./tweet-detail.model.js";
 import { buildTweetDetail } from "./tweet-detail.view.js";
 
 // user esta imagen para el detalle del tweet si el tweet obtenido por sparrest no tiene imagen.
 // https://demofree.sirv.com/nope-not-here.jpg
 
 export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
+  const handleRemoveTweetButton = () => {};
+
   try {
     const tweet = await getTweetDetail(tweetId);
     tweetDetailContainer.innerHTML = buildTweetDetail(tweet);
@@ -12,4 +14,16 @@ export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
     alert(error);
     window.location.href = "/";
   }
+
+  try {
+    const userData = await getUserData();
+    handleRemoveTweetButton();
+  } catch (error) {}
+
+  /**
+   * obtener datos del usuario que ha iniciado sesión
+   * comparar el id de usuario logado con el id de usuario del tweet
+   * si es el mismo, crear un botón de borrado
+   * cuando el botón se pulsa, confirmar acción y borrar el tweet.
+   */
 };
